833e711aeaab9c629128e0656579d86fae5e8c36
vjvl95 <vjvl95@naver.com>
2022-03-16 18:03:39 +0900

    fect : Project 컴포넌트 추가
    Project 컴포넌트 추가
    버튼 추가

--- front/src/components/project/Project.js
+++ front/src/components/project/Project.js
@@ -1,9 +1,43 @@
+import {useEffect, useState} from 'react'
+import ProjectCard from "./ProjectCard"
+import ProjectEdit from "./ProjectEdit";
+import { Card, Row, Button, Col } from "react-bootstrap";
+import * as Api from '../../api';
 
-function Project(){
-    return (<div>
-    <p>gdww</p>
-    </div>
-    )
+
+function Project({portfolioOwnerId,isEditable}){
+    const [isEditing, setIsEditing] = useState(false);
+    // useState 훅을 통해 user 상태를 생성함.
+    const [projectList, setProjectList] = useState(null);
+
+    useEffect(()=>{
+        Api.get('projectlist',portfolioOwnerId).then((res)=>setProjectList(res.data))
+    },[portfolioOwnerId])
+
+    return <Card className='mb-2 ms-3 mr-5' style={{}}>
+    <ProjectCard
+    setIsEditing={setIsEditing}
+    projectList={projectList}
+    isEditable={isEditable}
+    />
+
+    {isEditing && (
+        <ProjectEdit
+        projectList={projectList}
+          setIsEditing={setIsEditing}
+          setProjectList={setProjectList}
+        />
+      )}
+    </Card>
+    
+    
+     
+
+
+
+
+
+
 }
 
-export default Project;
+export default Project
--- front/src/components/project/ProjectCard.js
+++ front/src/components/project/ProjectCard.js
@@ -2,11 +2,16 @@ import { useNavigate } from "react-router-dom";
 import { Card, Row, Button, Col } from "react-bootstrap";
 
 
-function ProjectCard(){
+function ProjectCard({setIsEditing,project,isEditable}){
+
 
     return <Card >
     <Card.Body>
-        <Card.title>프로젝트</Card.title>
+        <Card.Title>프로젝트</Card.Title>
+        <Card.Body>
+          <button type="button" class="btn btn-primary" onClick={()=>setIsEditing(true)}>+</button>
+        </Card.Body>
+        
     </Card.Body>
   </Card>
 }
