2217eaa850b4b86a5e9463e92433965d0afe2be9
KA <song.kyunga@gmail.com>
2022-03-16 15:39:53 +0900

    fetch: Award.js 추가
    - create 구현
    - findByAwardId 구현
    - findByUserId 구현
    - update 구현

---
+++ back/src/db/models/Award.js
@@ -0,0 +1,33 @@
+import { AwardModel } from "../schemas/award";
+
+class Award {
+  static async create({ newAward }) {
+    const createdNewAward = await AwardModel.create(newAward);
+    return createdNewAward;
+  }
+
+  static async findByAwardId({ award_id }) {
+    const award = await AwardModel.findOne({ id: award_id });
+    return award;
+  }
+
+  static async findByUserId({ user_id }) {
+    const awards = await AwardModel.find({ user_id });
+    return awards;
+  }
+
+  static async update({ award_id, fieldToUpdate, newValue }) {
+    const filter = { id: award_id };
+    const update = { [fieldToUpdate]: newValue };
+    const option = { returnOriginal: false };
+
+    const updatedAward = await AwardModel.findOneAndUpdate(
+      filter,
+      update,
+      option
+    );
+    return updatedAward;
+  }
+}
+
+export { Award };
