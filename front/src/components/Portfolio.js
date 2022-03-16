16c0c3416fba972fb6dab488765ec477c8439b3b
songgaram <pager1436@gmail.com>
2022-03-16 12:04:24 +0900

    test : 커밋 테스트 2
    커밋 테스트 두번째

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -62,8 +62,6 @@ function Portfolio() {
         </Col>
         <Col>
           <div style={{ textAlign: "center" }}>
-            {/* < Award />
-            < Education /> */}
             학력 목록, 수상이력 목록, 프로젝트 목록, 자격증 목록 만들기
           </div>
         </Col>
