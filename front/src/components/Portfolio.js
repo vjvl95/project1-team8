059206f4b8752bbb83f4abd4ebdf7f4a021b092e
vjvl95 <vjvl95@naver.com>
2022-03-16 10:53:41 +0900

    commit temple 추가

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -61,7 +61,6 @@ function Portfolio() {
           />
         </Col>
         <Col>
-
           <div style={{ textAlign: "center" }}>
             학력 목록, 수상이력 목록, 프로젝트 목록, 자격증 목록 만들기
           </div>
