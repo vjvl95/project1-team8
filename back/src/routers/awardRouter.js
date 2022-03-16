7829b93601215ccd6d0c55ddc3958bc7b7d361b4
KA <song.kyunga@gmail.com>
2022-03-16 16:17:15 +0900

    fetch: awardRouter.js 추가
    - awardRouter.js 추가
    - POST /award/create 구현

---
+++ back/src/routers/awardRouter.js
@@ -0,0 +1,34 @@
+import is from "@sindresorhus/is";
+import { Router } from "express";
+import { login_required } from "../middlewares/login_required";
+import { awardService } from "../services/awardService";
+
+const awardRouter = Router();
+
+awardRouter.post("/award/create", async function (req, res, next) {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
+    }
+
+    // req (request) 에서 데이터 가져오기
+    const user_id = req.body.user_id;
+    const title = req.body.title;
+    const description = req.body.description;
+
+    // 위 데이터를 유저 db에 추가하기
+    const newAward = await awardAuthService.addAward({
+      user_id,
+      title,
+      description,
+    });
+
+    res.status(201).json(newAward);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export { awardRouter };
