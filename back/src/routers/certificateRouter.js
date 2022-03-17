6d8381144a829fb7808238ae0d7d0dcba31d3f0a
KA <song.kyunga@gmail.com>
2022-03-17 10:27:42 +0900

    feat: routers/certificateRouter.js 추가
    - certificate Router 파일 생성

---
+++ back/src/routers/certificateRouter.js
@@ -0,0 +1,11 @@
+import is from "@sindresorhus/is";
+import { Router } from "express";
+import { login_required } from "../middlewares/login_required";
+import { certificateService } from "../services/certificateService";
+
+const certificateRouter = Router();
+certificateRouter.use(login_required);
+
+
+
+export { certificateRouter };
