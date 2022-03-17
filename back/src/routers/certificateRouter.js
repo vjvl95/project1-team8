e97768191ac4c6aa154514dc38f4552e6c48ce90
KA <song.kyunga@gmail.com>
2022-03-17 10:29:17 +0900

    feat: POST certificate/create 기능 구현
    - certificate 생성하는 기능 구현

--- back/src/routers/certificateRouter.js
+++ back/src/routers/certificateRouter.js
@@ -6,6 +6,36 @@ import { certificateService } from "../services/certificateService";
 const certificateRouter = Router();
 certificateRouter.use(login_required);
 
+certificateRouter.post(
+  "/certificate/create", 
+  async function (req, res, next) {
+    try {
+      if (is.emptyObject(req.body)) {
+        throw new Error(
+          "headers의 Content-Type을 application/json으로 설정해주세요"
+        );
+      }
+
+      // req (request) 에서 데이터 가져오기
+      const user_id = req.body.user_id;
+      const title = req.body.title;
+      const description = req.body.description;
+      const when_date = req.body.when_date;
+
+      // 위 데이터를 유저 db에 추가하기
+      const newCertificate = await certificateService.addCertificate({
+        user_id,
+        title,
+        description,
+        when_date
+      });
+
+      res.status(201).json(newCertificate);
+    } catch (error) {
+      next(error);
+    }
+});
+
 
 
 export { certificateRouter };
