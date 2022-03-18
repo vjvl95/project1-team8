28a47e0b35b5a38e28b21ad503a4af8ccbeea8ba
songgaram <pager1436@gmail.com>
2022-03-19 08:56:04 +0900

    remove : EducationCard 폴더 삭제

--- front/src/components/education/Education.js
+++ front/src/components/education/Education.js
@@ -1,11 +1,11 @@
 import React, { useCallback, useState, useLayoutEffect } from "react";
-import { Card } from "react-bootstrap";
+import { Card, Row, Col, Button } from "react-bootstrap";
 
 // import * as Api from "../../api";
 import * as Api from "../../api";
 
-import EducationCard from "./EducationCard";
-import EducationEditForm from "./EducationEditForm";
+import EducationList from "./EducationList";
+import EducationAddForm from "./EducationAddForm";
 
 const Education = ({ portfolioOwnerId, isEditable }) => {
   // useState 훅을 통해 isEditing 상태를 생성함.
@@ -27,16 +27,26 @@ const Education = ({ portfolioOwnerId, isEditable }) => {
   return (
     <>
       <Card>
-        <EducationCard
-          portfolioOwnerId={portfolioOwnerId}
-          setIsEditing={setIsEditing}
-          isEditable={isEditable}
-          educations={educations}
-          getEducationList={getEducationList}
-        />
+        <Card.Body>
+          <Card.Title className="mb-3">학력</Card.Title>
+          <EducationList
+            portfolioOwnerId={portfolioOwnerId}
+            isEditable={isEditable}
+            educations={educations}
+            getEducationList={getEducationList}
+          />
+
+          {isEditable && (
+            <Row className="mt-3 text-center mb-4">
+              <Col sm={{ span: 20 }}>
+                <Button onClick={() => setIsEditing(true)}>+</Button>
+              </Col>
+            </Row>
+          )}
+        </Card.Body>
 
         {isEditing && (
-          <EducationEditForm
+          <EducationAddForm
             portfolioOwnerId={portfolioOwnerId}
             setIsEditing={setIsEditing}
             getEducationList={getEducationList}
--- front/src/components/education/EducationAddForm.js
+++ front/src/components/education/EducationAddForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Button, Form, Card, Col, Row } from "react-bootstrap";
 import * as Api from "../../api";
 
-const EducationEditForm = ({
+const EducationAddForm = ({
   portfolioOwnerId,
   setIsEditing,
   getEducationList,
@@ -103,4 +103,4 @@ const EducationEditForm = ({
   );
 };
 
-export default EducationEditForm;
+export default EducationAddForm;
