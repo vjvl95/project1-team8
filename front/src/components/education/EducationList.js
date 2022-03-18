8c262b221cfa8f89b8e5ec748e2b34e15dd6fcbf
songgaram <pager1436@gmail.com>
2022-03-18 16:30:18 +0900

    feat : getEducationList 함수 추가
    useCallback 사용하여 props로 넘겨줌

--- front/src/components/education/EducationList.js
+++ front/src/components/education/EducationList.js
@@ -1,6 +1,6 @@
 import { Card, Container, Row, Button, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import * as Api from "../../api";
 
 import EducationItem from "./EducationItem";
@@ -8,27 +8,30 @@ import EducationItem from "./EducationItem";
 const EducationList = ({ portfolioOwnerId, isEditable }) => {
   const [educations, setEducations] = useState([]);
 
-  useEffect(() => {
+  const getEducationList = useCallback(() => {
     // "educationlist/:user_id" 로 GET 요청을 하고, users를 response의 data로 세팅함.
-    Api.get("educationlist", portfolioOwnerId).then((res) =>
-      setEducations(res.data)
-    );
-  }, [educations]);
+    Api.get(`educationlist/${portfolioOwnerId}`).then((res) => {
+      const { data } = res;
+      setEducations(data);
+    });
+  }, [portfolioOwnerId]);
+
+  useEffect(() => {
+    getEducationList();
+  }, [getEducationList]);
 
   return (
     <>
       {educations.map((education) => {
         return (
-          <Container>
-            <EducationItem
-              key={education.id}
-              school={education.school}
-              major={education.major}
-              position={education.position}
-              id={education.id}
-              isEditable={isEditable}
-            />
-          </Container>
+          <EducationItem
+            key={education.id}
+            school={education.school}
+            major={education.major}
+            position={education.position}
+            id={education.id}
+            isEditable={isEditable}
+          />
         );
       })}
     </>
