bc7a7caeec481343cf59b376c96c4ce354f3c196
songgaram <pager1436@gmail.com>
2022-03-22 13:25:03 +0900

    fix : ProjectEditForm.js 수정 완료

---
+++ front/src/components/project/ProjectListItem.js
@@ -0,0 +1,59 @@
+import { Col, Button, Row } from "react-bootstrap";
+import { useState } from "react";
+import ProjectEditForm from "./ProjectEditForm";
+
+function ProjectListItem({ id, isEditable, item }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState(item.title);
+  const [newDescription, setNewDescription] = useState(item.description);
+  const [newFromDate, setNewFromDate] = useState(new Date(item.from_date));
+  const [newToDate, setNewToDate] = useState(new Date(item.to_date));
+
+  return (
+    <>
+      {isEditing ? (
+        <ProjectEditForm
+          setIsEditing={setIsEditing}
+          itemId={id}
+          itemTitle={newTitle}
+          itemDescription={newDescription}
+          itemFromDate={newFromDate}
+          itemToDate={newToDate}
+          setNewTitle={setNewTitle}
+          setNewDescription={setNewDescription}
+          setNewFromDate={setNewFromDate}
+          setNewToDate={setNewToDate}
+        />
+      ) : (
+        <Row className="align-items-center mb-3">
+          <Col>
+            <span>{newTitle}</span>
+            <br />
+            <span className="text-muted">{newDescription}</span>
+            <br />
+            <span className="text-muted">
+              {newFromDate.toISOString().substring(0, 10)} ~{" "}
+              {newToDate.toISOString().substring(0, 10)}
+            </span>
+          </Col>
+
+          {isEditable && (
+            <Col className="col-lg-1">
+              <Button
+                variant="outline-info"
+                size="sm"
+                onClick={() => {
+                  setIsEditing(true);
+                }}
+              >
+                편집
+              </Button>
+            </Col>
+          )}
+        </Row>
+      )}
+    </>
+  );
+}
+
+export default ProjectListItem;
