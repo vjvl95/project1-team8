d061cb34c08d3f254865ade8b60c9271b64a4ccc
HONG <loans93@gmail.com>
2022-03-24 10:49:17 +0900

    blankpage

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 import { User } from "./models/User";
-import { Award } from "./models/Award";
-import { Certificate } from "./models/Certificate";
-import { Education } from "./models/Education";
-import { Project } from "./models/Project";
+
 
 const DB_URL =
   process.env.MONGODB_URL ||
@@ -20,4 +17,4 @@ db.on("error", (error) =>
   console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
-export { User, Award, Certificate, Education, Project };
+export { User };
