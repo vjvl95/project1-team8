c06311813d605d40615679a1f6a0b533fa77f529
songgaram <pager1436@gmail.com>
2022-03-26 15:25:19 +0900

    feat : spinner / bookmark 오류 수정

--- front/src/components/user/Bookmark.js
+++ front/src/components/user/Bookmark.js
@@ -1,39 +1,64 @@
-import React, { useContext,useEffect, useState } from 'react';
-import { Container, Row,Spinner } from 'react-bootstrap';
-import * as Api from '../../api';
-import UserCard from './UserCard';
+import React, { useContext, useEffect, useState } from "react";
+import { Container, Row, Spinner } from "react-bootstrap";
+import * as Api from "../../api";
+import UserCard from "./UserCard";
 import { BookmarkListContext } from "../../App";
 
-
-function Bookmark(){
+function Bookmark() {
   const [users, setUsers] = useState([]);
-  const {setBookmarklist}=useContext(BookmarkListContext)
-  const [isLoading,setIsLoading]=useState(false)
+  const { setBookmarklist } = useContext(BookmarkListContext);
+  const [isLoading, setIsLoading] = useState(false);
 
-
-    useEffect(() => {
-        async function getBookmark(){
-          const bookmarklist=await Api.get('user/bookmarklist')
-          const res=await Api.get('user/bookmarklist_data')
-          setBookmarklist(bookmarklist.data)
-          setUsers(res.data)
-      }
-      getBookmark()
-      setIsLoading(true)
-      }, []);
-    function bookmark_user()
-    {
-      return users.length===0?<h1 style={{marginTop:"400px", marginLeft:"650px"}}>북마크한 포토폴리오가 없습니다</h1>  
-      : users.map((user) => (<UserCard bookmarkMargin={"100px"}key={user?.id} user={user} isNetwork/>))
+  useEffect(() => {
+    async function getBookmark() {
+      const bookmarklist = await Api.get("user/bookmarklist");
+      const res = await Api.get("user/bookmarklist_data");
+      setBookmarklist(bookmarklist.data);
+      setUsers(res.data);
     }
-    return (
-        <Container fluid>
-          <Row className='jusify-content-center'>
-            {isLoading  ?<div style={{display:'flex',flexDirection:"column",justifyContent:"center", alignItems:"center"}}><Spinner animation="border" variant="primary" style={{marginTop:"300px",justifyContent:"center"}} /> </div>: <div>3</div>}
-          </Row>
-        </Container>
-      );
+    getBookmark();
+    setIsLoading(true);
+  }, []);
+  function bookmark_user() {
+    return users.length === 0 ? (
+      <h1 style={{ marginTop: "400px", marginLeft: "650px" }}>
+        북마크한 포토폴리오가 없습니다
+      </h1>
+    ) : (
+      users.map((user) => (
+        <UserCard
+          bookmarkMargin={"100px"}
+          key={user?.id}
+          user={user}
+          isNetwork
+        />
+      ))
+    );
+  }
+  return (
+    <Container fluid>
+      <Row className="jusify-content-center">
+        {isLoading === true ? (
+          bookmark_user()
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Spinner
+              animation="border"
+              variant="primary"
+              style={{ marginTop: "300px", justifyContent: "center" }}
+            />
+          </div>
+        )}
+      </Row>
+    </Container>
+  );
 }
 
-
-export default Bookmark 
+export default Bookmark;
