7ccaac6461be8555f9461fec8fb4e1189fec4cef
vjvl95 <vjvl95@naver.com>
2022-03-26 22:30:50 +0900

    feat:로그인 후 네트워크 이동
    찐 들간다~~~~

--- front/src/components/user/LoginForm.js
+++ front/src/components/user/LoginForm.js
@@ -54,7 +54,7 @@ function LoginForm() {
       });
 
       // 기본 페이지로 이동함.
-      navigate("/", { replace: true });
+      navigate("/network", { replace: true });
     } catch (err) {
       console.log("이메일 또는 비밀번호가 유효하지 않습니다.");
     }
