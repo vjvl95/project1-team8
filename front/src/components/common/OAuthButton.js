6f7a987be6a6afa6080abc7d5ab448cf3db7588a
Junha <prodopark15@gmail.com>
2022-03-26 14:51:24 +0900

    fix : oAuth url코드 기능구현

--- front/src/components/common/OAuthButton.js
+++ front/src/components/common/OAuthButton.js
@@ -1,7 +1,13 @@
 const OAuthButton = () => {
   const onClick = () => {
+    const protocol = window.location.protocol;
+    const hostName = window.location.hostname;
+    const port = window.location.port;
+    let url = protocol + '//' + hostName + (port ? ':' + port : '') + '/test';
     window.location.href =
-      'https://accounts.google.com/o/oauth2/auth?client_id=917034651706-impnf3jg1g4hh6des3ea5iuibtmggms8.apps.googleusercontent.com&redirect_uri=http://localhost:3000/test&response_type=token&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile';
+      'https://accounts.google.com/o/oauth2/auth?client_id=917034651706-impnf3jg1g4hh6des3ea5iuibtmggms8.apps.googleusercontent.com&redirect_uri=' +
+      url +
+      '&response_type=token&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile';
   };
   return <button onClick={onClick}>oAuth 버튼</button>;
 };
