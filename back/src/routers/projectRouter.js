a4d493234211555d9ea61e11d5e8ff31ac0ad740
KA <song.kyunga@gmail.com>
2022-03-26 14:28:28 +0900

    refact: project controller 추가
    - router에서 함수부분 controller로 이동

--- back/src/routers/projectRouter.js
+++ back/src/routers/projectRouter.js
@@ -1,109 +1,18 @@
-import is from "@sindresorhus/is";
 import { Router } from "express";
 import { login_required } from "../middlewares/login_required";
-import { projectService } from "../services/projectService";
-import { headerError } from "../utils/errorMessages"
+import { projectController } from "../controllers/projectController"
 
 const projectRouter = Router();
 projectRouter.use(login_required);
 
-projectRouter.post("/projects/project", async (req, res, next) => {
-  try {
-    if (is.emptyObject(req.body)) {
-      throw new Error(
-        headerError
-      );
-    }
-    // jwt토큰에서 추출된 사용자 id를 가지고 db에서 사용자 정보를 찾음.
-    const user_id = req.currentUserId;
-    // req (request) 에서 데이터 가져오기
-    const title = req.body.title;
-    const description = req.body.description;
-    const from_date = req.body.from_date;
-    const to_date = req.body.to_date;
+projectRouter.post("/projects/project", projectController.addProject);
 
-    // 위 데이터를 project db에 추가하기
-    const newProject = await projectService.addProject({
-      user_id,
-      title,
-      description,
-      from_date,
-      to_date,
-    });
+projectRouter.get("/projects/:id", projectController.getProject);
 
-    if (newProject.errorMessage) {
-      throw new Error(newProject.errorMessage);
-    }
+projectRouter.put("/projects/:id", projectController.setProject);
 
-    res.status(201).end();
-  } catch (error) {
-    next(error);
-  }
-});
+projectRouter.get("/projectlist/:user_id", projectController.getProjectList);
 
-projectRouter.get('/projects/:id', async (req, res, next) => {
-  try {
-    const projectId = req.params.id;
-    const foundProject = await projectService.getProject({ projectId });
-    if (foundProject.errorMessage) {
-      throw new Error(foundProject.errorMessage);
-    }
-
-    res.status(200).json(foundProject);
-  } catch (error) {
-    next(error);
-  }
-});
-
-projectRouter.put('/projects/:id', async (req, res, next) => {
-  try {
-    const projectId = req.params.id;
-    const title = req.body.title ?? null;
-    const description = req.body.description ?? null;
-    const from_date = req.body.from_date ?? null;
-    const to_date = req.body.to_date ?? null;
-
-    const toUpdate = { title, description, from_date, to_date };
-
-    const updatedProject = await projectService.setProject({
-      projectId,
-      toUpdate,
-    });
-
-    if (updatedProject.errorMessage) {
-      throw new Error(updatedProject.errorMessage);
-    }
-
-    res.status(200).json(updatedProject);
-  } catch (error) {
-    next(error);
-  }
-});
-
-projectRouter.get('/projectlist/:user_id', async (req, res, next) => {
-  try {
-    const { user_id } = req.params;
-    const foundList = await projectService.getProjectList({ user_id });
-
-    res.status(200).json(foundList);
-  } catch (error) {
-    next(error);
-  }
-});
-
-projectRouter.delete('/projects/:id', async function (req, res, next) {
-  try {
-    const projectId = req.params.id;
-    const deletedResult = await projectService.deleteProject({ projectId });
-
-    if (deletedResult.errorMessage) {
-      throw new Error(deletedResult.errorMessage);
-    }
-
-    res.status(200).end();
-  } catch (error) {
-    next(error);
-  }
-});
+projectRouter.delete("/projects/:id", projectController.deleteProject);
 
 export { projectRouter };
