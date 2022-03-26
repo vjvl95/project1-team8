206ae035c8ea74d202df7c0f547020bfcb55c73e
songgaram <pager1436@gmail.com>
2022-03-26 14:39:49 +0900

    refact : 변경사항

--- front/src/components/user/UserTable.js
+++ front/src/components/user/UserTable.js
@@ -3,14 +3,12 @@ import * as Api from "../../api";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Network.module.css";
-import {FaMedal} from "react-icons/fa"
+import { FaMedal } from "react-icons/fa";
 import { BookmarkListContext } from "../../App";
 
 function UserTable() {
   const [top3, setTop3] = useState([]);
   const navigate = useNavigate();
-  const [toggle, setToggle] = useState();
-  const { bookmarklist } = useContext(BookmarkListContext);
 
   useEffect(() => {
     async function getTop3() {
@@ -22,51 +20,16 @@ function UserTable() {
     console.log(top3);
   }, []);
 
-  useEffect(() => {
-
-    if (bookmarklist !== undefined) {
-      bookmarklist?.includes(user?.id) ? setToggle(true) : setToggle(false);
-    }
-    if (isEditable === false) {
-      bookmarklist?.includes(portfolioOwnerId)
-        ? setToggle(true)
-        : setToggle(false);
-    }
-  }, [bookmarklist]);
-
-
-
-  function medal(num){
-    return (
-              num===1 ?<FaMedal size="30"style={{color:"gold"}}/>
-              :num===2?< FaMedal size="30" style={{color:"#D3D3D3"}}/>
-              :<FaMedal size="30" style={{color:"#cd7f32"}}/>
-    )
+  function medal(num) {
+    return num === 1 ? (
+      <FaMedal size="30" style={{ color: "gold" }} />
+    ) : num === 2 ? (
+      <FaMedal size="30" style={{ color: "#D3D3D3" }} />
+    ) : (
+      <FaMedal size="30" style={{ color: "#cd7f32" }} />
+    );
   }
 
-  function bookmark(bookmarked){
-
-    return (
-      <>
-        {toggle ? (
-          <AiTwotoneStar
-            style={{ fontSize: "30px", marginLeft: "90px" }}
-            onClick={toggleHander}
-          />
-        ) : (
-          <AiOutlineStar
-            style={{ fontSize: "30px", marginLeft: "90px" }}
-            onClick={toggleHander}
-          />
-        )}
-        <span
-          style={{ fontSize: "20px", marginLeft: "5px", marginTop: "15px" }}
-        >
-          {bookmarked}
-        </span>
-      </>
-
-  }
   return (
     <div
       style={{
@@ -100,7 +63,7 @@ function UserTable() {
               <td className="table_style">{medal(index + 1)}</td>
               <td className="table_style">{name}</td>
               <td className="table_style">{email}</td>
-              <td className="table_style">{bookmark(bookMarked)}</td>
+              <td className="table_style">{bookMarked}</td>
               <td className="table_style">
                 <Button onClick={() => navigate(`/users/${id}`)}>
                   상세보기
